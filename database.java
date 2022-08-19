a1437a9b6a84185626148b2363e28ef70d6b075d
Tyrel Cruz <111546308+tyrelcruz@users.noreply.github.com>
2022-08-19 10:17:35 +0800

    Add files via upload

---
+++ database.java
@@ -0,0 +1,49 @@
+public class database {
+    public static void main(String[] args) {
+
+        // int - stores integers (whole numbers), without decimals, such as 123 or -123
+        int myAge = 19;
+        int myFavoritenumber = 27;
+
+        // float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+        float myGradeInClass = 4.00f;
+        float Atomicweightofoxygen = 15.99f;
+
+        // char - stores single characters, such as 'a' or 'B'. Char values are
+        // surrounded by single quotes
+        char myMiddleInitial = 'P';
+        char myFavoriteletter = 'V';
+
+        // boolean - stores values with two states: true or false
+        boolean DoIhavecovid = false;
+        boolean amIhealthy = true;
+
+        // String - stores text, such as "Hello". String values are surrounded by double
+        // quotes
+        String myFirstName = "Tyrel";
+        String myCT = "vVv";
+        String myOrg = "venividivici esports";
+
+        // If you don't want others (or yourself) to overwrite existing values, use the
+        // final keyword (this will declare the variable as "final" or "constant", which
+        // means unchangeable and read-only)
+        final String myLastName = "Cruz";
+        final String myIGN = "STLLR";
+        final String myTeam = "Fuego";
+;
+        System.out.println(myAge);
+        System.out.println(myFavoritenumber);
+        System.out.println(myGradeInClass);
+        System.out.println(Atomicweightofoxygen);
+        System.out.println(myMiddleInitial);
+        System.out.println(myFavoriteletter);
+        System.out.println(DoIhavecovid);
+        System.out.println(amIhealthy);
+        System.out.println(myFirstName);
+        System.out.println(myCT);
+        System.out.println(myOrg);
+        System.out.println(myLastName);
+        System.out.println(myIGN);
+        System.out.println(myTeam);
+    }
+}
